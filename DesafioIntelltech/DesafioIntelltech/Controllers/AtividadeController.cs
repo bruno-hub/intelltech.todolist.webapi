727402d8386c3261f0b9a14e04eb06746fa176c7
bruno-hub <bobrunoho1@gmail.com>
2017-02-16 14:37:09 -0200

    Primeira parte

---
+++ DesafioIntelltech/DesafioIntelltech/Controllers/AtividadeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DesafioIntelltech.Models;
+
+namespace DesafioIntelltech.Controllers
+{
+    public class AtividadeController : ApiController
+    {
+        Atividade[] atividades = new Atividade[]
+        {
+            new Atividade(1, "Aprender C#", "Estudar a sintaxe", "16/02/2017", "09:00", 1),
+            new Atividade(2, "Pensar em C#", "Aprender datas em C#", "16/02/2017", "09:00", 0),
+            new Atividade(3, "Almoçar", "Ir almoçar em algum lugar", "16/02/2017", "12:00", 2)
+        };
+        
+        // /api/atividades
+        public IEnumerable<Atividade> GetAllAtividades()
+        {
+            return atividades;
+        }
+
+        // /api/atividades/?id
+        public IHttpActionResult GetAtividade(int id)
+        {
+            var atividade = atividades.FirstOrDefault((a) => a.id == id);
+            Console.WriteLine(atividade);
+            if (atividade == null)
+                return NotFound();
+
+            return Ok(atividade);
+        }
+    }
+}
---
+++ DesafioIntelltech/DesafioIntelltech/Models/Atividade.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesafioIntelltech.Models
+{
+    public class Atividade
+    {
+        public int id { get; set; }
+        private string titulo { get; set; }
+        private string descricao { get; set; }
+        private string data { get; set; }
+        private string horario { get; set; }
+
+        private int posSituacao = 0;
+        private string[] situacao = { "Não iniciada", "Em andamento", "Concluída" };
+
+        public Atividade(int id, string titulo, string descricao, string data, string horario, int posSituacao)
+        {
+            this.id = id;
+            this.titulo = titulo;
+            this.descricao = descricao;
+            this.data = data;
+            this.horario = horario;
+            this.posSituacao = posSituacao;
+        }
+
+        public string getSituacao()
+        {
+            return this.situacao[posSituacao];
+        }
+        
+        public void setSituacao(int posSituacao)
+        {
+            this.posSituacao = posSituacao;
+        }
+    }
+}
