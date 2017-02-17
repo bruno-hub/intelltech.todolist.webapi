7fce74018feb2c71194167d1bdd52ed463ada085
bruno-hub <bobrunoho1@gmail.com>
2017-02-17 10:54:05 -0200

    Fim primeira parte

--- DesafioIntelltech/DesafioIntelltech/Controllers/AtividadeController.cs
+++ DesafioIntelltech/DesafioIntelltech/Controllers/AtividadeController.cs
@@ -7,30 +7,37 @@ using DesafioIntelltech.Models;
 
 namespace DesafioIntelltech.Controllers
 {
-    public class AtividadeController : ApiController
-    {
-        Atividade[] atividades = new Atividade[]
-        {
-            new Atividade(1, "Aprender C#", "Estudar a sintaxe", "16/02/2017", "09:00", 1),
-            new Atividade(2, "Pensar em C#", "Aprender datas em C#", "16/02/2017", "09:00", 0),
-            new Atividade(3, "Almoçar", "Ir almoçar em algum lugar", "16/02/2017", "12:00", 2)
-        };
-        
-        // /api/atividade
-        public IEnumerable<Atividade> GetAllAtividades()
-        {
-            return atividades;
-        }
+	public class AtividadeController : ApiController
+	{
+		Atividade[] atividades = new Atividade[]
+		{
+			new Atividade(1, "Aprender C#", "Estudar a sintaxe", "16/02/2017", "09:00", 1),
+			new Atividade(2, "Pensar em C#", "Aprender datas em C#", "16/02/2017", "10:00", 0),
+			new Atividade(3, "Almoçar", "Ir almoçar em algum lugar", "16/02/2017", "12:00", 2)
+		};
+		
+		// /api/atividade
+		public IEnumerable<Atividade> GetAllAtividades()
+		{
+			return atividades;
+		}
 
-        // /api/atividade/?id
-        public IHttpActionResult GetAtividade(int id)
-        {
-            var atividade = atividades.FirstOrDefault((a) => a.id == id);
-            Console.WriteLine(atividade);
-            if (atividade == null)
-                return NotFound();
+		// /api/atividade/?id
+		public IHttpActionResult GetAtividade(int id)
+		{
+			var atividade = atividades.FirstOrDefault((a) => a.id == id);
+			Console.WriteLine(atividade);
+			if (atividade == null)
+				return NotFound();
 
-            return Ok(atividade);
-        }
-    }
+			return Ok(atividade);
+		}
+
+		public IEnumerable<Atividade> GetAtividadesPorSituacao(string situacao)
+		{
+			return atividades.Where(
+				(p) => string.Equals(p.getSituacao(), situacao,
+					StringComparison.OrdinalIgnoreCase));
+		}
+	}
 }
--- DesafioIntelltech/DesafioIntelltech/Models/Atividade.cs
+++ DesafioIntelltech/DesafioIntelltech/Models/Atividade.cs
@@ -5,35 +5,35 @@ using System.Web;
 
 namespace DesafioIntelltech.Models
 {
-    public class Atividade
-    {
-        public int id { get; set; }
-        public string titulo { get; set; }
-        public string descricao { get; set; }
-        public string data { get; set; }
-        public string horario { get; set; }
+	public class Atividade
+	{
+		public int id { get; set; }
+		public string titulo { get; set; }
+		public string descricao { get; set; }
+		public string data { get; set; }
+		public string horario { get; set; }
 
-        private int posSituacao = 0;
-        private string[] situacao = { "Não iniciada", "Em andamento", "Concluída" };
+		private int posSituacao = 0;
+		private string[] situacao = { "Não iniciada", "Em andamento", "Concluída" };
 
-        public Atividade(int id, string titulo, string descricao, string data, string horario, int posSituacao)
-        {
-            this.id = id;
-            this.titulo = titulo;
-            this.descricao = descricao;
-            this.data = data;
-            this.horario = horario;
-            this.posSituacao = posSituacao;
-        }
+		public Atividade(int id, string titulo, string descricao, string data, string horario, int posSituacao)
+		{
+			this.id = id;
+			this.titulo = titulo;
+			this.descricao = descricao;
+			this.data = data;
+			this.horario = horario;
+			this.posSituacao = posSituacao;
+		}
 
-        public string getSituacao()
-        {
-            return this.situacao[posSituacao];
-        }
-        
-        public void setSituacao(int posSituacao)
-        {
-            this.posSituacao = posSituacao;
-        }
-    }
+		public string getSituacao()
+		{
+			return this.situacao[posSituacao];
+		}
+		
+		public void setSituacao(int posSituacao)
+		{
+			this.posSituacao = posSituacao;
+		}
+	}
 }
