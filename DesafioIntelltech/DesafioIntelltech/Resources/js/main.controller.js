0e923f548d05b10ee037858e1310b87b79b2e1a4
bruno-hub <bobrunoho1@gmail.com>
2017-02-17 16:43:11 -0200

    Fim primeira parte

---
+++ DesafioIntelltech/DesafioIntelltech/Resources/js/main.controller.js
@@ -0,0 +1,35 @@
+﻿(function () {
+	'use strict';
+
+	angular
+	  .module('app.MainController', [])
+	  .controller('MainController', function ($scope, $log, $http) {
+
+	  	var vm = this;
+
+	  	$log.debug('eteste');
+
+	  	vm.atividade = "";
+	  	vm.find = find;
+		vm.search = "";
+		function find(id) {
+			$log.debug(vm.search);
+	  		$http.get('api/atividade/' + id).then(function (response) {
+	
+
+	  			vm.atividade = response.data;
+	  		}, function (response) {
+
+	  		});
+	  	}
+
+	  	vm.atividades = [];
+	  	$http.get('api/atividade').then(function (response) {
+	  		
+	  		vm.atividades = response.data;
+	  	}, function (response) {
+
+	  	});
+
+	  });
+})();
